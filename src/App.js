aae7f0379c6a412ef3a264874f9f949a49c0db54
rowan-khamis <rowankhamis66@gmail.com>
2024-07-14 19:48:42 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import './Components/CustomerTable/customerTable.js'
+import CustomerTable from './Components/CustomerTable/customerTable.js';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <CustomerTable />
     </div>
   );
 }
